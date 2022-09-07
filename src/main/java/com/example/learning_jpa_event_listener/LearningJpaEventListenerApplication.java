123cd89c23b7dcd3ee59daf18b190bee9cc484fa
radek-nowak <radeknowak1999@gmail.com>
2022-09-07 16:24:34 +0200

    Initial commit

---
+++ src/main/java/com/example/learning_jpa_event_listener/LearningJpaEventListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_jpa_event_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpaEventListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpaEventListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning_jpa_event_listener/LearningJpaEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_jpa_event_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaEventListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
